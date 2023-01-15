9ea0be1a9db022074532345bd11ac0567162f2c5
meequrox <117393211+meequrox@users.noreply.github.com>
2023-01-15 16:35:29 +0700

    Use CLI11 library to read arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
+#include <CLI/App.hpp>
+#include <CLI/CLI.hpp>
+#include <CLI/Formatter.hpp>
 #include <iostream>
 
 #include "ConfigReader.hpp"
 
-int main() {
-    ConfigReader cr(std::string("/home/") + std::getenv("USER") +
-                    "/.local/share/goodvibes/stations.xml");
+int main(int argc, char** argv) {
+    CLI::App app{"goodvibes2transistor"};
+
+    // std::string("/home/") + std::getenv("USER") + "/.local/share/goodvibes/stations.xml"
+    std::string configPath;
+
+    // TODO: add --find option, which attempts to find the station.xml file and print its path
+    app.add_option("-i", configPath, "stations.xml file path")->check(CLI::ExistingPath)->required();
+    CLI11_PARSE(app, argc, argv);
+
+    ConfigReader cr(configPath);
     cr.printStations();
 
     return 0;

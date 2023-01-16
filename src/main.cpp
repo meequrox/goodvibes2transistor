e1541d18270755f1591228b67364c5afe51a5880
meequrox <117393211+meequrox@users.noreply.github.com>
2023-01-16 20:54:50 +0700

    main.cpp: redesign options & flags, add --guess flag

--- src/main.cpp
+++ src/main.cpp
@@ -9,30 +9,49 @@
 int main(int argc, char** argv) {
     CLI::App app{"goodvibes2transistor"};
 
-    // std::string("/home/") + std::getenv("USER") + "/.local/share/goodvibes/stations.xml"
     std::string configPath;
-    bool archive = false;
-    bool verbose = false;
 
-    // TODO: add --find option, which attempts to find the station.xml file and print its path
-    app.add_option("-i", configPath, "stations.xml file path")->check(CLI::ExistingFile)->required();
-    app.add_flag("-v,--verbose", verbose, "be verbose (stations + dumps)");
-    app.add_flag("-a,--archive", archive, "archive collection to backup zip file");
+    CLI::Option* oInput = app.add_option("-i,--input", configPath,
+                                         "stations.xml file path\n"
+                                         "use -g instead if you don't know its location")
+                              ->group("Input")
+                              ->check(CLI::ExistingFile);
+    CLI::Option* fGuess = app.add_flag("-g,--guess",
+                                       "program will find stations.xml file by itself\n"
+                                       "don't use with -i flag")
+                              ->group("Input")
+                              ->excludes(oInput);
+
+    CLI::Option* fArchive =
+        app.add_flag("-a,--archive", "archive collection to backup zip file")->group("Output");
+    CLI::Option* fVerbose =
+        app.add_flag("-v,--verbose", "be verbose (stations + dumps)")->group("Output");
 
     CLI11_PARSE(app, argc, argv);
 
-    if (verbose) std::cout << app.config_to_str() << std::endl;
+    if (!fGuess->count() && !oInput->count()) {
+        app.exit(CLI::Error("", "Either the -i option or the -g flag must be provided."));
+        return EXIT_FAILURE;
+    }
+
+    if (fVerbose->count()) std::cout << app.config_to_str() << std::endl;
+
+    if (fGuess->count()) {
+        configPath =
+            std::string("/home/") + std::getenv("USER") + "/.local/share/goodvibes/stations.xml";
+        std::cout << "Guess mode: config is probably at " << configPath << std::endl;
+    }
 
     try {
         ConfigReader cr(configPath);
-        cr.printStations(verbose);
+        cr.printStations(fVerbose->count());
 
         Converter converter(cr);
-        converter.dumpCollection(archive, verbose);
+        converter.dumpCollection(fArchive->count(), fVerbose->count());
 
     } catch (const std::invalid_argument& e) {
-        std::cout << e.what() << ", exitting..." << std::endl;
+        std::cout << e.what() << std::endl;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }

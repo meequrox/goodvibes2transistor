0c51698997794e38510470450f8c1c6409c68ac0
meequrox <117393211+meequrox@users.noreply.github.com>
2023-01-31 20:00:39 +0700

    BookmarksReader: make methods virtual

--- src/BookmarksReader.hpp
+++ src/BookmarksReader.hpp
@@ -1,27 +1,21 @@
 #ifndef BOOKMARKS_READER_HPP
 #define BOOKMARKS_READER_HPP
 
-#include <tinyxml2.h>
-
 #include <string>
 #include <vector>
 
 class BookmarksReader {
-   private:
-    tinyxml2::XMLDocument doc;
-    const tinyxml2::XMLElement* stations;
-
    public:
-    BookmarksReader(const std::string& path);
-
     typedef std::vector<std::pair<std::string, std::string>> stationsList;
 
     /* Returns vector with pairs:
      * pair.first is station name
      * pair.second is station uri */
-    stationsList getStationsList() const;
+    virtual stationsList getStationsList() const = 0;
 
-    void printStations(bool verbose) const;
+    virtual void printStations(bool verbose) const = 0;
+
+    virtual ~BookmarksReader() = default;
 };
 
 #endif  // BOOKMARKS_READER_HPP

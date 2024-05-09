348d7c3f16198a85db0716871780004cbea6bbc7
meequrox <117393211+meequrox@users.noreply.github.com>
2024-05-10 03:23:37 +0700

    Fix build error

--- radio2transistor/xmake.lua
+++ radio2transistor/xmake.lua
@@ -1,23 +1,33 @@
 target("radio2transistor")
     set_kind("binary")
     add_files("app/*.cpp")
-    set_default(true)
+    -- add_headerfiles("app/*.hpp")
 
     add_deps("converter", "reader", "zipper")
-    add_packages("cli11", "nlohmann_json", "minizip")
+    add_packages("cli11")
+    add_packages("nlohmann_json")
+    add_packages("minizip")
+
+    set_default(true)
 
 
 target("converter")
     set_kind("static")
     add_files("converter/*.cpp")
-    add_includedirs("$(projectdir)/radio2transistor", {public = true})
+    add_headerfiles("converter/*.hpp")
 
-    add_packages("nlohmann_json", "uuid_v4")
+    add_packages("nlohmann_json", {public = true})
+    add_packages("uuid_v4", {public = true})
+
+    add_includedirs("$(projectdir)/radio2transistor", {public = true})
 
 
 target("reader")
     set_kind("static")
     add_files("reader/*.cpp")
-    add_includedirs("$(projectdir)/radio2transistor", {public = true})
+    add_headerfiles("reader/*.hpp")
 
-    add_packages("nlohmann_json", "tinyxml2")
+    add_packages("nlohmann_json", {public = true})
+    add_packages("tinyxml2", {public = true})
+
+    add_includedirs("$(projectdir)/radio2transistor", {public = true})
